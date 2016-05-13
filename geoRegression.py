3c6b369a6d8d4bcba1b487096ee8cc5dd7f66462
Jiajun <shenjiajun90@gmail.com>
2016-05-12 23:16:49 -0500

    Add set up variable for the function

--- geoRegression.py
+++ geoRegression.py
@@ -9,9 +9,48 @@
 
 import numpy as np
 import theano
-
+import theano.tensor as T
+import theano.typed_list
+import pandas as pd
 
 
 def train():
-    nRegions = 10
+    nRegions = 2
     adjacencyMatrix = np.zeros((nRegions, nRegions))
+
+    # The shape of the inputData is [nRregions, nx(p + 1)]
+    inputData = theano.typed_list.TypedListType(T.fmatrix)()
+
+    # The shape of the inputDataResponse is [nRegions, n]
+    inputDataResponse = theano.typed_list.TypedListType(T.fvector)()
+
+
+    coefficient = T.fmatrix()
+    loop, _ = theano.scan(fn = lambda i, tl:tl[i].sum(),non_sequences=[inputData], sequences=[theano.tensor.arange(nRegions, dtype = 'int64')])
+
+    #predictions = [T.dot(inputData[i], coefficient[i]) for i in range(nRegions)]
+    #rss = np.sum([(T.sqr(predictions[i] - inputDataResponse[i])).sum() for i in range(nRegions)])
+
+    output_function = theano.function([inputData], [loop])
+
+
+
+    loaded_data = [np.zeros((3, 5), dtype = np.float32) for i in range(2)]
+    #loded_data_label = [np.zeros(3) for i in range(2)]
+    #loaded_data_coeffcient = [np.zeros(5)]
+    print output_function(loaded_data)
+
+
+
+
+
+
+def main():
+    train()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
